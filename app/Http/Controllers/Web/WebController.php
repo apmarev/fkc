63836173b52dcfdbb8e94d22aeea241eeae2c5a3
apmarev <57302841+apmarev@users.noreply.github.com>
2022-05-21 15:11:03 +0200

    init

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -16,16 +16,41 @@ class WebController extends Controller {
     }
 
     public function getFunction(Request $request, $type) {
-        $custom = '';
-        $period = '';
+        $date = [];
+
         if($request->has('period')) {
             $period = $request->input('period');
             if($period == 'custom') {
-                Telegram::sendMessage([
-                    'chat_id' => 228519769,
-                    'text' => json_encode($request->all())
-                ]);
+                $date = [
+                    'from' => strtotime($request->input('date_from')),
+                    'to' => strtotime($request->input('date_to'))
+                ];
+            } else {
+                if($period == 'week') {
+                    $date = [
+                        'from' => strtotime(date("d.m.Y", strtotime('monday this week')) . "00.00.01"),
+                        'to' => time(),
+                        'test' => date("d.m.Y", strtotime('monday this week')),
+                    ];
+                } else if($period == 'day') {
+                    $date = [
+                        'from' => strtotime(date('d.m.Y') . "00.00.01"),
+                        'to' => time()
+                    ];
+                } else if($period == 'yesterday') {
+                    $date = [
+                        'from' => strtotime(date('d.m.Y', strtotime('yesterday')) . "00.00.01"),
+                        'to' => strtotime(date('d.m.Y', strtotime('yesterday')) . "23.59.59")
+                    ];
+                } else if($period == 'month') {
+
+                }
             }
+
+            Telegram::sendMessage([
+                'chat_id' => 228519769,
+                'text' => json_encode($date)
+            ]);
 
         }
         return $this->$type($period, $custom);
