b502d5986d20354e124d30f9d6c7cdc84933fc2c
apmarev <57302841+apmarev@users.noreply.github.com>
2022-05-21 15:00:27 +0200

    init

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -16,34 +16,39 @@ class WebController extends Controller {
     }
 
     public function getFunction(Request $request, $type) {
+        $custom = '';
+        $period = '';
         if($request->has('period')) {
-            Telegram::sendMessage([
-                'chat_id' => 228519769,
-                'text' => $request->input('period')
-            ]);
+            $period = $request->input('period');
+            if($period == 'custom') {
+                Telegram::sendMessage([
+                    'chat_id' => 228519769,
+                    'text' => json_encode($request->all())
+                ]);
+            }
 
         }
-        return $this->$type();
+        return $this->$type($period, $custom);
     }
 
     /**
      * Виджет «Анализ продаж»
      */
-    public function salesAnalysis() {
+    public function salesAnalysis($period = '', $custom = '') {
         return view('reports.salesAnalysis', [ 'items' => $this->reports->salesAnalysis() ]);
     }
 
     /**
      * Виджет «Сделки по менеджерам»
      */
-    public function dealsByManager() {
+    public function dealsByManager($period = '', $custom = '') {
         return view('reports.dealsByManager', [ 'items' => $this->reports->dealsByManager() ]);
     }
 
     /**
      * Виджет «Продажи по менеджерам»
      */
-    public function salesByManager() {
+    public function salesByManager($period, $custom = '') {
 
     }
 
