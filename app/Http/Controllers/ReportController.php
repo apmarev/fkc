577775d29ac0170ea3bff199436cd5c10c829d4f
apmarev <57302841+apmarev@users.noreply.github.com>
2022-07-25 10:27:02 +0200

    init

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -31,17 +31,6 @@ class ReportController extends Controller {
             $t++;
         }
 
-        $result = [];
-
-        for($i=1;;$i++) {
-            $query = "/'leads'?page={$i}&limit=250{$filter}";
-            $res = $this->amoGet($query);
-
-            $result[] = $result;
-        }
-
-        return $result;
-
         $leads = $this->amo->getAllListByFilter('leads', "{$filter}");
 
         return [
@@ -159,6 +148,17 @@ class ReportController extends Controller {
             $filter .= "&filter[id][{$t}]={$l['entity_id']}";
             $t++;
         }
+
+        $result = [];
+
+        for($i=1;;$i++) {
+            $query = "/leads?page={$i}&limit=250{$filter}";
+            $res = $this->amo->amoGet($query);
+
+            $result[] = $result;
+        }
+
+        return $result;
 
         $leads = $this->amo->getAllListByFilter('leads', "{$filter}");
         return $leads;
