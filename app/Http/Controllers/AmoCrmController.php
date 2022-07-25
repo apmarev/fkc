024dee4f67c187bec872766714242b1b4c764f45
apmarev <57302841+apmarev@users.noreply.github.com>
2022-07-25 16:28:37 +0200

    init

--- app/Http/Controllers/AmoCrmController.php
+++ app/Http/Controllers/AmoCrmController.php
@@ -50,6 +50,10 @@ class AmoCrmController extends Controller {
             else
                 return [];
         } else {
+            Telegram::sendMessage([
+                'chat_id' => 228519769,
+                'text' => json_encode($data)
+            ]);
             return [];
         }
 //        try {
