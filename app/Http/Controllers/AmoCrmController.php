7a270258680b2e0e56285c3ee09ef38c8c334b16
apmarev <57302841+apmarev@users.noreply.github.com>
2022-07-25 10:47:36 +0200

    init

--- app/Http/Controllers/AmoCrmController.php
+++ app/Http/Controllers/AmoCrmController.php
@@ -42,7 +42,7 @@ class AmoCrmController extends Controller {
     }
 
     public static function getIsSetList($data, string $type) {
-        return $data['_embedded'][$type];
+        return $data['_embedded'][$type] ?? [];
     }
 
     public static function getIsSetListCustomFields($data): array {
